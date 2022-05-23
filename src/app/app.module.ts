55fad437b718cf387baee185fe4e07cce624482d
nshah <nshah@ssnc-corp.global>
2022-05-23 10:51:06 +0500

    naveed first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { PageFooterComponent } from './page-footer/page-footer.component';
+import { DisplayContentComponent } from './display-content/display-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    PageHeaderComponent,
+    PageFooterComponent,
+    DisplayContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [MainMenuComponent,
+    PageHeaderComponent,
+    PageFooterComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
